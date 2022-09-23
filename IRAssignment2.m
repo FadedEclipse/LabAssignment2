2abe2f23b857035f4f3ceec7cb3fe63b03a504d1
CGBruh <13533878@student.uts.edu.au>
2022-09-23 16:08:40 +1000

    Updated Environment + Models

--- IRAssignment2.m
+++ IRAssignment2.m
@@ -1,13 +1,21 @@
 clc
 clf
-clear all 
-hold on
+hold on 
+clear all
+axis ([-1.25 1.25 -2 1 0 1.28]);
 
-camlight;
+robot = UR3();
 
-axis ([-1 1 -1 1 0 1])
+initialState = [1.5708 -1.5708 0 -1.5708 0 0];
+robot.model.animate(initialState);
 
-Kuka = KUKA();
 
-UR3 = UR3();
+kuka = KUKA();
 
+surf([1,1;-1,-1],[0.99,0.99;0.99,0.99],[0.5,1.28;0.5,1.28],'CData',imread('barwall.jpg'),'FaceColor','texturemap');
+surf([-3,-3;+3,+3],[-3,+3;-3,+3],[0.001,0.001;0.001,0.001],'CData',imread('woodfloor.png'),'FaceColor','texturemap');
+surf([1.25,1.25;-1.25,-1.25],[1,1;1,1],[0,1.28;0,1.28],'CData',imread('woodfloor.png'),'FaceColor','texturemap');
+PlaceObject('bar.ply', [0,-0.7,0]);
+PlaceObject('Bench.ply', [0,0.7,0]);
+PlaceObject('safetyfence.ply', [1,0,0]);
+PlaceObject('safetyfence.ply', [-1,0,0]);
