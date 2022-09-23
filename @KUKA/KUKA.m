2feccc9b3cd076757328449c5316639b21ae6910
Harley <92080366+FadedEclipse@users.noreply.github.com>
2022-09-23 11:33:41 +1000

    Update KUKA.m

--- @KUKA/KUKA.m
+++ @KUKA/KUKA.m
@@ -8,8 +8,6 @@ classdef KUKA < handle
    
     end
     
-    % this is a test
-    
     methods%% Class for KUKA robot simulation
         function self = KUKA(toolModelAndTCPFilenames)
             if 0 < nargin
