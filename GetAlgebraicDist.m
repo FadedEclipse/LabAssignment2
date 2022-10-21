0847f611143cf8b854f6204ee939bce36d94eaa1
CGBruh <13533878@student.uts.edu.au>
2022-10-21 15:15:42 +1100

    More comments

--- GetAlgebraicDist.m
+++ GetAlgebraicDist.m
@@ -1,5 +1,10 @@
 function algebraicDist = GetAlgebraicDist(points, centerPoint, radii)
 
+% if algebraicDist < 1 point is inside the ellipsoid 
+%if algebraicDist = 1 point is on the surface of ellispoid
+%if algebraicDist >1 point is outside of ellipsoid 
+
+%The general equation for a sphere in 3D space is given as:
 algebraicDist = ((points(:,1)-centerPoint(1))/radii(1)).^2 ...
               + ((points(:,2)-centerPoint(2))/radii(2)).^2 ...
               + ((points(:,3)-centerPoint(3))/radii(3)).^2;
--- KUKASphereCollisionDetection.m
+++ KUKASphereCollisionDetection.m
@@ -1,33 +1,47 @@
 function KUKASphereCollisionDetection(robot,q,transformedVertices)
 
-
+%center points for all ellipsoids - dervied via using teach and ploting
+%each ellipsoid on the robots respective links
 centerPoint = [0,0,0.15; 0,0,0; -0.125,0,0; 0,0,0.125; 0,-0.1,0; 0,0,-0.15; 0,0,-0.15];
 
+%radii for each ellipsoid in the x y and z directions - determined so that
+%the robot can detect an appropriate distance for when it will come into
+%collision - takes into account dimensions of each joint 
 radii = [0.1,0.1,0.3; 0.1,0.3,0.1; 0.25,0.1,0.1; 0.1,0.1,0.25; 0.1,0.2,0.1; 0.1,0.1,0.25; 0.1,0.1,0.25];
 
- for i = 1:7
+%creates and plots each ellipsoid on the robots respective links using the ellipsoid function
+ for i = 1:7 % one ellipsoid for each link including robot base 
 a = centerPoint(i,:);
 b = radii(i,:);
+%Function returns ellipsoid points
 [X,Y,Z] = ellipsoid( a(1), a(2), a(3), b(1), b(2), b(3) );
 
 
-    robot.model.points{i} = [X(:),Y(:),Z(:)];
+    robot.model.points{i} = [X(:),Y(:),Z(:)]; % set these ellipoid points, to be points on the robot 
     warning off
-    robot.model.faces{i} = delaunay(robot.model.points{i});    
+    robot.model.faces{i} = delaunay(robot.model.points{i});  % triangulate points to create ellipsoid face definitions   
     warning on;
 end  
     
 tr = zeros(4,4,robot.model.n+1);
 tr(:,:,1) = robot.model.base;
 L = robot.model.links;
+%allows us to find which link is in collision with each link ellipsoid that
+%contain points - find transfroms from base to end effector 
 for i = 1 : robot.model.n
-    tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+    tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha); % find location and transform of each joint and end effector in the environment
 end    
-    
+
+% step through each joint/link and check for collisions
 for i = 1: size(tr,3)    
-cubePointsAndOnes = [inv(tr(:,:,i)) * transformedVertices']';
-updatedCubePoints = cubePointsAndOnes(:,1:3);
-algebraicDist = GetAlgebraicDist(updatedCubePoints, centerPoint(i,:), radii(i,:));
+cubePointsAndOnes = [inv(tr(:,:,i)) * transformedVertices']'; %multiple points by inverse joint transform
+updatedCubePoints = cubePointsAndOnes(:,1:3); 
+algebraicDist = GetAlgebraicDist(updatedCubePoints, centerPoint(i,:), radii(i,:)); %check algebraic distance using function
+%returns number of points within the ellipsoid 
+
+% if algebraicDist < 1 point is inside the ellipsoid 
+%if algebraicDist = 1 point is on the surface of ellispoid
+%if algebraicDist >1 point is outside of ellipsoid 
 pointsInside = find(algebraicDist < 1);
 
 
